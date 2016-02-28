a3d8ee29ac7378cff963f0f07b75d2128e23798d
alvatraz <alex.alvarez0010@gmail.com>
2016-02-27 22:40:53 -0600

    Update index.aspx.cs

--- FinalProject/FinalProject/index.aspx.cs
+++ FinalProject/FinalProject/index.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FinalProject.Models;
 
 namespace FinalProject
 {
@@ -12,6 +13,31 @@ namespace FinalProject
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        
+        protected void btnSignIn_Click(object sender, EventArgs e)
+        {
+            using (StoreContent context = new StoreContent())
+            {
+                var user = (from c in context.Customer
+                            where c.UserName == txtUsername.Text && c.Password == txtPassword.Text
+                            select c).FirstOrDefault();
+
+                if (user != null)
+                {
+                    Session["LoggedInId"] = user.Id.ToString();
+                    Session["FirstName"] = user.FirstName;
+                    Session["LastName"] = user.LastName;
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "alert",
+                "alert('Login credentials accepted!');", true);
+                }
+                else
+                {
+                    //Display invalid login
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "err-msg",
+                "alert('Incorrect login credentials given!');", true);
+                }
+            }
         }
     }
 }
