68f29a4133ecbdf377160858f3766fd919cdf082
alvatraz <alex.alvarez0010@gmail.com>
2016-03-07 19:46:59 -0600

    Search page

---
+++ FinalProject/FinalProject/Search.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Actions.Master" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="FinalProject.Search" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphPageName" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cphTop" runat="server">
+    <asp:Label runat="server" ID="lblSearch" Text="Found "></asp:Label>
+    <hr />
+    <asp:Table runat="server" ID="tblSearch"></asp:Table>
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="cphBottom" runat="server">
+</asp:Content>
---
+++ FinalProject/FinalProject/Search.aspx.cs
@@ -0,0 +1,47 @@
+﻿using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalProject
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        string search;
+        int results = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            search = Request.QueryString["query"];
+            using (StoreContent context = new StoreContent())
+            {
+                var products = (from p in context.Products
+                                where p.ProductName.ToLower().Contains(search.ToLower()) || p.ProductDescription.ToLower().Contains(search.ToLower())
+                                select p).ToList();
+                if(products!= null)
+                {
+                    foreach(Products p in products) { 
+                        TableRow row = new TableRow();
+
+                        TableCell cell = new TableCell();
+                        cell.Text = "<img src=\"" + p.image + "\" />";
+                        row.Cells.Add(cell);
+
+                        cell = new TableCell();
+                        cell.Text = p.ProductName;
+                        row.Cells.Add(cell);
+
+                        cell = new TableCell();
+                        cell.Text = p.ProductDescription;
+                        row.Cells.Add(cell);
+                        tblSearch.Rows.Add(row);
+                        results++;
+                    }
+                }
+            }
+            lblSearch.Text+=results+" result(s) for '"+search+"'";
+        }
+    }
+}
