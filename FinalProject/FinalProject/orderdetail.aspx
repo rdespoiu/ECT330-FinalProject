4e972f07bf9cd1b5ac553645cfc1cc3ad67ab581
rdespoiu <roberto.despoiu@gmail.com>
2016-03-12 18:20:11 -0600

    Working on detailed order page

---
+++ FinalProject/FinalProject/orderdetail.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Actions.Master" AutoEventWireup="true" CodeBehind="orderdetail.aspx.cs" Inherits="FinalProject.orderdetail" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="/CSS/orderdetail.css" />
+</asp:Content>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="cphPageName" runat="server">
+    <p><asp:Label ID="lblOrder" runat="server" Text="Placeholder test"></asp:Label></p>
+</asp:Content>
+
+
+<asp:Content ID="Content3" ContentPlaceHolderID="cphTop" runat="server">
+    <asp:Table ID="tblOrderDetail" runat="server">
+        <asp:TableHeaderRow>
+            <asp:TableCell><b>Product Name</b></asp:TableCell>
+            <asp:TableCell><b>Quantity</b></asp:TableCell>
+            <asp:TableCell><b>Price</b></asp:TableCell>
+        </asp:TableHeaderRow>
+    </asp:Table>
+</asp:Content>
+
+
+<asp:Content ID="Content4" ContentPlaceHolderID="cphBottom" runat="server">
+    <div id="orderDetailBottom">
+        <div id="orderDetailBottomLeft">
+            <asp:Label ID="lblTotal" runat="server" Text="$0.00"></asp:Label>
+        </div>
+
+        <div id="orderDetailBottomRight">
+            <asp:Button ID="btnBack" runat="server" Text="Back to Orders" CssClass="backButton" />
+        </div>
+    </div>
+</asp:Content>
---
+++ FinalProject/FinalProject/orderdetail.aspx.cs
@@ -0,0 +1,95 @@
+﻿using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalProject
+{
+    public partial class orderdetail : System.Web.UI.Page
+    {
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["LoggedInId"] == null || Request.QueryString["Id"] == null)
+            {
+                Response.Redirect("index.aspx");
+            } else
+            {
+                populateData();
+            }
+
+            
+            
+        }
+
+        protected void populateData()
+        {
+            int orderId = Int32.Parse(Request.QueryString["Id"].ToString());
+            int userId = Int32.Parse(Session["LoggedInId"].ToString());
+
+            using (StoreContent context = new StoreContent())
+            {
+
+                var orderItems = from c in context.OrderItem
+                                 where c.OrderID == orderId && c.CustomerID == userId
+                                 select c;
+                
+
+                if (orderItems != null)
+                {
+                    foreach (OrderItem item in orderItems)
+                    {
+
+                        var product = (from c in context.Products
+                                       where c.Id == item.ProductID
+                                       select c).FirstOrDefault();
+
+                        TableRow row = new TableRow();
+                        TableCell cell;
+
+                        cell = new TableCell();
+                        HyperLink link = new HyperLink();
+                        link.NavigateUrl = "product.aspx?Id=" + product.Id;
+                        link.Text = product.ProductName;
+                        cell.Controls.Add(link);
+                        row.Cells.Add(cell);
+
+                        cell = new TableCell();
+                        cell.Text = item.Quantity.ToString();
+                        row.Cells.Add(cell);
+
+                        cell = new TableCell();
+                        cell.Text = "$" + (product.UnitPrice * item.Quantity).ToString("N2") + " (" + product.UnitPrice.ToString("N2") + " ea)";
+                        row.Cells.Add(cell);
+
+
+
+                        
+                    }
+
+                    var orderTotal = (from c in context.Orders
+                                      where c.Id == orderId
+                                      select c).FirstOrDefault();
+
+                    if (orderTotal != null)
+                    {
+                        lblTotal.Text = orderTotal.SubTotal.ToString("N2");
+                    }
+
+                }
+
+                
+
+
+            }
+
+
+
+
+        }
+    }
+}
